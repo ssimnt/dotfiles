40fadeb11992c65f864c5cae92aabb5a406f137a
ssimnt <simon@simontilby.co.uk>
2022-07-06 22:15:04 +0100

    Update .conf links

--- 2-zsh.sh
+++ 2-zsh.sh
@@ -10,8 +10,8 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | zsh
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | zsh
 
 # Copy over config files
-curl https://github.com/ssimnt/dotfiles/blob/main/.p10k.zsh --output .p10k.zsh
-curl https://github.com/ssimnt/dotfiles/blob/main/.zshrc --output .zshrc
-curl https://github.com/ssimnt/dotfiles/blob/main/.tmux.conf --output .tmux.conf
+curl https://raw.githubusercontent.com/ssimnt/dotfiles/main/.p10k.zsh --output .p10k.zsh
+curl https://raw.githubusercontent.com/ssimnt/dotfiles/main/.zshrc --output .zshrc
+curl https://raw.githubusercontent.com/ssimnt/dotfiles/main/.tmux.conf --output .tmux.conf
 
 # then prefix I to load tmux plugins
