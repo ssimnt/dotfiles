5059583385918467185b4053de24770823a573fd
ssimnt <simon@simontilby.co.uk>
2022-12-10 20:27:26 +0000

    Add theme & paging options to bat

--- aliases.zsh
+++ aliases.zsh
@@ -1,4 +1,4 @@
-alias cat='batcat'
+alias cat='batcat --theme=Nord --paging=never'
 alias -g G='| grep -i'
 alias serve='python3 -m http.server'
 alias lt='exa -TL2 --icons --group-directories-first'
